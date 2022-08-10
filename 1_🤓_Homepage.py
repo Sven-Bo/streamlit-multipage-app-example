bbcbfd4ccbc9085c8beb6169d26925490552ea15
Sarang Kharche <sarangkharche@gmail.com>
2022-08-10 12:01:30 +0100

    added contact form

--- 1_🤓_Homepage.py
+++ 1_🤓_Homepage.py
@@ -16,3 +16,26 @@ submit = st.button("Submit")
 if submit:
     st.session_state["my_input"] = my_input
     st.write("You have entered: ", my_input)
+
+
+st.sidebar.header(":mailbox: Get In Touch With Me!")
+
+contact_form = """
+<form action="https://formsubmit.co/curiousish1@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.sidebar.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/styles.css")
